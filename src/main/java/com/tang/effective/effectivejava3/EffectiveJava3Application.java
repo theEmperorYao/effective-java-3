8b1b820e8486e3c78aace4bbf7e973394ebdc85c
tangyao <1013654193@qq.com>
2021-12-29 14:57:38 +0800

    第一次提交

---
+++ src/main/java/com/tang/effective/effectivejava3/EffectiveJava3Application.java
@@ -0,0 +1,13 @@
+package com.tang.effective.effectivejava3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EffectiveJava3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EffectiveJava3Application.class, args);
+    }
+
+}
